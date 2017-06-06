8365c298c7df09f03fc1b476c20f280af7fb380d
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-06-05 20:56:06 -0600

    adds model testing, two feature tests now failing

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,6 @@
 class Link < ApplicationRecord
   validates :url, :url => true
+  validates :url, uniqueness: true
 
   def self.top10
     order(read: :desc).limit(10)
--- spec/requests/api/v1/links_request_spec.rb
+++ spec/requests/api/v1/links_request_spec.rb
@@ -13,6 +13,19 @@ describe "Links API" do
     expect(Link.count).to eq(1)
   end
 
+  it "can update link" do
+    link_one = Link.create(url: "https://gist.github.com/case-eee/77380935d29fdb971fd9ab50a43586e3", read:2)
+    link_params = {url: "https://gist.github.com/case-eee/77380935d29fdb971fd9ab50a43586e3"}
+    
+    post "/api/v1/links", params: {link: link_params}
+    link = Link.last
+    
+    assert_response :success
+    expect(response).to be_success
+    expect(link.url).to eq(link_params[:url])
+    expect(link.read).to eq(3)
+  end
+
   it "can return a list of the top-10 most-read URLs" do
     create_list(:link, 10, read: 1)
     create_list(:link, 2,read: 10)
